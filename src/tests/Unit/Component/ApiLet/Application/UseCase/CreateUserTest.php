f0f5686b6d4e279d59d0f75f6140009f6a38a40a
jordi.llobetdomingo <jordi.llobetdomingo@students.salle.url.edu>
2020-05-01 18:53:42 +0200

    Install PHPUnit Configuration and create Test for CreateUser

---
+++ src/tests/Unit/Component/ApiLet/Application/UseCase/CreateUserTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Component\ApiLet\Application\UseCase;
+
+use App\Component\ApiLet\Application\Request\CreateUserRequest;
+use App\Component\ApiLet\Application\UseCase\CreateUser;
+use App\Component\ApiLet\Domain\User\Repository\UserRepository;
+use PHPUnit\Framework\TestCase;
+
+class CreateUserTest extends TestCase
+{
+    private $userRepository;
+    private $createUserRequest;
+    private $createUser;
+
+    protected function setUp(): void
+    {
+        $this->userRepository = $this->createMock(UserRepository::class);
+        $this->createUserRequest = $this->createMock(CreateUserRequest::class);
+        $this->createUser = new CreateUser($this->userRepository);
+    }
+
+    /** @test */
+    public function shouldSaveAUser()
+    {
+        $this->givenACreateUserRequest();
+        $this->thenSaveUserWhenExecuteUseCase();
+    }
+
+    private function givenACreateUserRequest(): void
+    {
+        $this->createUserRequest->method("getId")->willReturn(1);
+        $this->createUserRequest->method("getFirstName")->willReturn("Joan");
+        $this->createUserRequest->method("getLastName")->willReturn("Garcia");
+        $this->createUserRequest->method("getEmail")->willReturn("example@example.com");
+    }
+
+    private function thenSaveUserWhenExecuteUseCase()
+    {
+        $this->userRepository->expects($this->once())->method("save");
+        $this->createUser->execute($this->createUserRequest);
+    }
+
+}
