a333129ada6c942a5f9f236898bc754d342b3cee
jordi.llobetdomingo <jordi.llobetdomingo@students.salle.url.edu>
2020-05-24 20:00:47 +0200

    Add Pill entity and doctrine mapping

--- src/src/Component/Domain/Chat/Entity/Issue.php
+++ src/src/Component/Domain/Chat/Entity/Issue.php
@@ -4,12 +4,13 @@
 namespace LetEmTalk\Component\Domain\Chat\Entity;
 
 
-use LetEmTalk\Component\Domain\Entity\Chat\Room;
+use LetEmTalk\Component\Domain\Chat\Entity\Room;
 
 class Issue
 {
     private int $id;
     private Room $room;
     private string $title;
+    private Pill $firstPill;
 
 }
--- src/src/Component/Domain/Chat/Entity/Pill.php
+++ src/src/Component/Domain/Chat/Entity/Pill.php
@@ -4,7 +4,13 @@
 namespace LetEmTalk\Component\Domain\Chat\Entity;
 
 
+use LetEmTalk\Component\Domain\User\Entity\User;
+
 class Pill
 {
-
+    private int $id;
+    private Issue $issue;
+    private string $text;
+    private User $author;
+    private \DateTime $created;
 }
