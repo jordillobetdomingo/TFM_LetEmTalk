87613798dcfc8e01f809958013963d99432498a7
jordi.llobetdomingo <jordi.llobetdomingo@students.salle.url.edu>
2020-05-30 17:36:13 +0200

    Add password constraints

--- src/src/Component/Domain/Authentication/ValueObject/Password.php
+++ src/src/Component/Domain/Authentication/ValueObject/Password.php
@@ -6,10 +6,25 @@ namespace LetEmTalk\Component\Domain\Authentication\ValueObject;
 
 class Password
 {
+    const MINIMUM_LENGTH_PASSWORD = 8;
+
     private string $password;
 
     public function __construct(string $password)
     {
+        // Minimum length
+        if (strlen($password) < self::MINIMUM_LENGTH_PASSWORD) {
+            throw new \InvalidArgumentException();
+        }
+        // Password no contain a digit
+        if (!preg_match('/\d/', $password)) {
+            throw new \InvalidArgumentException();
+        }
+        // Password no contain a letter
+        if (!preg_match('/[a-zA-Z]/', $password))
+        {
+            throw new \InvalidArgumentException();
+        }
         $this->password = $password;
     }
 
