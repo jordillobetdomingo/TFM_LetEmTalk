f7a12ace8ee45e1e5cbe6ecf96278276d9237d79
jordi.llobetdomingo <jordi.llobetdomingo@students.salle.url.edu>
2020-06-05 22:00:31 +0200

    Add AdminPermission Entity and Doctrine Mapping

---
+++ src/src/Component/Domain/Authorization/Entity/AdminPermission.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace LetEmTalk\Component\Domain\Authorization\Entity;
+
+
+use LetEmTalk\Component\Domain\User\Entity\User;
+
+class AdminPermission
+{
+    private User $user;
+    private bool $adminPermission;
+
+}
