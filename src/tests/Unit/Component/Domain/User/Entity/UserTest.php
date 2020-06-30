d9687f9d99c722f8728190600ed36988801aec7c
jordi.llobetdomingo <jordi.llobetdomingo@students.salle.url.edu>
2020-06-30 08:48:05 +0200

    UnitTest ValueObject

---
+++ src/tests/Unit/Component/Domain/User/Entity/UserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LetEmTalk\Tests\Unit\Component\Domain\User\Entity;
+
+use LetEmTalk\Component\Domain\User\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+
+}
